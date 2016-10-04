52e474600f440b3c7327a386c6a02b5cb2656d77
Steve Norledge <steve.norledge@alces-software.com>
2016-10-04 14:42:54 +0100

    - swap now on vdb

--- install/bin/stage1.sh
+++ install/bin/stage1.sh
@@ -135,4 +135,4 @@ EOF
 sed -i -e "s/^PermitRootLogin.*$/PermitRootLogin without-password/g" /etc/ssh/sshd_config
 
 #swap mount
-echo "/dev/vdd        swap    swap    defaults 0 0" >> /etc/fstab
+echo "/dev/vdb        swap    swap    defaults 0 0" >> /etc/fstab
